b8bc0903d6fab33aa34e278eed91318426500345
siddharth sharma <48689008+sidd512@users.noreply.github.com>
2019-03-18 22:41:47 +0530

    labels problem successfully rectified

--- init.java
+++ init.java
@@ -6,11 +6,12 @@ class youtube
    JFrame f=new JFrame("MewBot.exe");//creating instance of JFrame 
    JLabel l1; 
    l1=new JLabel("YOUTUBE TO MP3");
-   l1.setBounds(50,50, 100,30);
+   l1.setBounds(50,50, 200,30);
    f.setLayout(null);  
    JButton b=new JButton("click me");//creating instance of JButton
    b.setBounds(130,100,100, 40);//x axis, y axis, width, height
    f.setVisible(true);          
+   f.add(l1);
    f.add(b);//adding button in JFrame            
    f.setSize(400,500);//400 width and 500 height  
    f.setLayout(null);//using no layout managers  
