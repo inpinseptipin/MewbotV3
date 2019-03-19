1be18427aae2280e28ecd169a5256542963b509e
satyarth arora <satyarth.arora@asu.apeejay.edu>
2019-03-19 14:30:01 +0530

    Switched from Swing to Java Fx

--- init.java
+++ init.java
@@ -1,79 +1,32 @@
-import javax.swing.*;  
-import java.awt.*;
-import java.awt.event.*;
-
-
-
-class youtube
-{  
+import javafx.application.Application;  
+import javafx.scene.Scene;  
+import javafx.scene.control.Label;  
+import javafx.scene.layout.StackPane;  
+import javafx.stage.Stage;  
   
-  static JProgressBar bar_1;
-
-  public static JLabel H1()
-  {
-    JLabel h;
-    h=new JLabel("Welcome to MewBot");
-    h.setBounds(200,250,10,10);
-    return h;
-  }
-
-  public static void Bar_1()
-  {
-    bar_1= new JProgressBar();
-    bar_1.setValue(0);
-    bar_1.setStringPainted(true);
+public class init extends Application {  
+  
     
-  }
+    
+    static Label l1;
+    static Label l2;
 
-  public static void fill()
-  {
-    int i=0;
-    try
-    {
-      while(i<=100)
-      {
-        bar_1.setValue(i+10);
-        Thread.sleep(1000);
-        i+=20;
-      }
+    @Override  
+    public void start(Stage primaryStage) throws Exception
+    {  
+        // TODO Auto-generated method stub  
+        l1=new Label("Welcome to MewBot");  
+        StackPane root = new StackPane();   
+        Scene scene=new Scene(root,300,300);  
+        root.getChildren().add(l1);  
+        primaryStage.setScene(scene);  
+        primaryStage.setTitle("Youtube To Mp3");  
+        primaryStage.show();  
+          
     }
 
-    catch(Exception e)
-    {
 
-    }
-  }
-
-  public static void main(String[] args)
-  {  
-    JFrame f=new JFrame("MewBot.exe"); //creating instance of JFrame
-
-    JPanel panel_1=new JPanel();
-    
-    Bar_1();
-
-    panel_1.add(bar_1);
-
-    panel_1.setBounds(10,300,400,50);
-    
-
-    JLabel h1=H1();
-
-      
-    JButton b=new JButton("click me");//creating instance of JButton
-    b.setBounds(130,100,100, 40);//x axis, y axis, width, height
-    f.setVisible(true);          
-    f.add(h1);
-    f.add(panel_1);
-    f.add(b);//adding button in JFrame            
-    f.setSize(400,500);//400 width and 500 height  
-    //f.setLayout(null);//using no layout managers  
-    f.setVisible(true);//making the frame visible 
-
-    fill();
-
-     
-  }  
-
-  
-}  
+    public static void main(String[] args) {  
+        launch(args);  
+    }  
+}
