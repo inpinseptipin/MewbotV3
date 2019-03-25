eff882daa8f64181a3bb69f7648a8a055739efc6
satyarth arora <satyarth.arora@asu.apeejay.edu>
2019-03-26 04:38:33 +0530

    MultiThread Support Added
    MultiThread Support added to Download and Conversion
    
    Solved GUI Not Responding Issue

--- init.java
+++ init.java
@@ -57,17 +57,74 @@ public class init extends Application
         return b;
     }
 
-    public void run_script()
+    EventHandler<ActionEvent> event= new EventHandler<ActionEvent>()
+    {
+        public void handle(ActionEvent e)
+        {
+           Runnable_1 r_1=new Runnable_1();
+           Thread t_1=new Thread(r_1);
+           t_1.start();
+            
+        }
+    };
+
+    
+
+    @Override                                                                
+
+    public void start(Stage primaryStage) throws Exception
+    {
+        t1=init_Text("Welcome to MewBot",0,-275);
+        t1.setFont(Font.font("Chocolate Dealer",45));
+        t1.setFill(Color.RED);
+               
+        b1=init_Button("Download and Convert",0,0);
+        
+        Image i = new Image("https://i0.wp.com/www.freepptbackgrounds.net/wp-content/uploads/2015/03/Listening-Music-Powerpoint-Templates.jpg?resize=806%2C605&ssl=1");
+        BackgroundImage bgi = new BackgroundImage(i,BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.DEFAULT,BackgroundSize.DEFAULT); 
+        Background bg = new Background(bgi);
+        
+        StackPane root = new StackPane();
+
+       	root.setBackground(bg);
+                      
+        Scene scene = new Scene(root,800,600);  
+
+        
+
+        b1.setOnAction(event);
+     
+        
+
+        root.getChildren().add(b1);
+        root.getChildren().add(t1);
+        primaryStage.setTitle("MewBot.exe");  
+        primaryStage.setScene(scene);    
+        primaryStage.show();  
+    }
+
+    public static void main(String[] args) 
+    {  
+        launch(args);  
+    }  
+}
+
+class Runnable_1 implements Runnable
+{
+	private int var;
+
+	public Runnable_1()
+	{
+		this.var=0;
+	}
+
+	public  void run()
     {
         Boolean flag=true;
         try
         { 
-            // Command to create an external process 
-            
-            String line;
-  
-            // Running the above command 
-            
+                       
+            String line;                   
             Process proc_1 = new ProcessBuilder("C:\\Users\\arsen\\Desktop\\MewBot.exe\\build\\Bin\\ripper.exe","C:\\Users\\arsen\\Desktop\\MewBot.exe\\build\\Bin\\music.txt").start();; 
             BufferedReader bri=new BufferedReader(new InputStreamReader(proc_1.getInputStream()));
             BufferedReader bre=new BufferedReader(new InputStreamReader(proc_1.getErrorStream()));
@@ -87,58 +144,13 @@ public class init extends Application
             System.out.println("Done");
 
         } 
-  
         catch (IOException e) 
         { 
             e.printStackTrace(); 
             System.out.println("In exception");
             flag=false;
         } 
-
-
     }
 
-    @Override                                                                
 
-    public void start(Stage primaryStage) throws Exception
-    {
-        t1=init_Text("Welcome to MewBot",0,-275);
-        t1.setFont(Font.font("Chocolate Dealer",45));
-        t1.setFill(Color.RED);
-               
-        b1=init_Button("Click me",0,0);
-        
-        Image i = new Image("https://i0.wp.com/www.freepptbackgrounds.net/wp-content/uploads/2015/03/Listening-Music-Powerpoint-Templates.jpg?resize=806%2C605&ssl=1");
-        BackgroundImage bgi = new BackgroundImage(i,BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.DEFAULT,BackgroundSize.DEFAULT); 
-        Background bg = new Background(bgi);
-        
-        StackPane root = new StackPane();
-
-       	root.setBackground(bg);
-                      
-        Scene scene = new Scene(root,800,600);  
-
-        EventHandler<ActionEvent> event= new EventHandler<ActionEvent>()
-        {
-            public void handle(ActionEvent e)
-            {
-                run_script();
-            }
-        };
-
-        b1.setOnAction(event);
-     
-        
-
-        root.getChildren().add(b1);
-        root.getChildren().add(t1);
-        primaryStage.setTitle("MewBot.exe");  
-        primaryStage.setScene(scene);    
-        primaryStage.show();  
-    }
-
-    public static void main(String[] args) 
-    {  
-        launch(args);  
-    }  
 }
